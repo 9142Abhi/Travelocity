e95efea65fba3729d4b1b84556b174897cca7c5f
Abhishek <abhisingh24082002@gmail.com>
2022-12-15 22:19:42 +0530

    adding day-3

--- travelocity/src/App.js
+++ travelocity/src/App.js
@@ -1,11 +1,14 @@
+// import AllRoutes from "./Router/AllRoutes";
 import Navbar from "./Components/Navbar";
 import "./App.css";
-
+import Home from "./Components/Home";
 function App() {
   return (
     <div className="App">
       {/* <p>This is the last thing</p> */}
       <Navbar />
+
+      <Home />
     </div>
   );
 }
---
+++ travelocity/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+
+function Home(){
+    
+
+    return (
+        <div style={{position:"absolute"}}>
+            
+        <img style={{width:"100%"}}
+         src="https://forever.travel-assets.com/flex/flexmanager/images/2021/06/25/TVLY_SeizeYourSomeday_lpheroB_1680x945_20210623.jpg?impolicy=fcrop&w=900&h=225&q=mediumHigh" alt="" />
+        
+        </div>
+    )
+}
+
+export default Home;
